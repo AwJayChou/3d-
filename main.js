a02fe68cac7f71cb44afd1c4f40b2fa92d695c6a
jaychou <jaychou.rjd@aliyun.com>
2023-02-02 18:50:24 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import App from './App'
+// 3d 木鱼小程序wx12b587eaa497207e
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/index/index.vue
@@ -0,0 +1,70 @@
+<template>
+	<view>
+		<web-view :webview-styles="webviewStyles" :src="webViewSrc"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+				webviewStyles: {
+					progress: {
+						color: '#FF3333'
+					}
+				},
+				// webViewSrc: 'https://dev-xingmou.mvtlabs.com/muyu',
+				webViewSrc: 'http://192.168.0.106:1024/muyu'
+				// http://192.168.0.106:1024/muyu
+			}
+		},
+		onLoad() {
+			setTimeout(() => {
+				uni.setNavigationBarTitle({
+					title: '3d木鱼'
+				})
+			}, 2000)
+		},
+		methods: {
+			setBool() {
+				this.isTest = !this.isTest
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.test {
+		width: 200upx;
+		height: 200upx;
+		background-color: aqua;
+		color: #8f8f94;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
